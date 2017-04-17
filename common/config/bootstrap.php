0b8651059f7be689b53a55acdebff37826c26841
trac.nguyen <trac.nguyen@wizeline.com>
2017-04-17 11:55:42 +0700

    Test the bootstrap

---
+++ common/config/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author: npbtrac@yahoo.com
+ * Date time: 4/17/17 11:42 AM
+ */
+
+// Defining root namespace to the root folder of application
+Yii::setAlias('root', dirname(dirname(__DIR__)));
+
+// Defining common namespace
+Yii::setAlias('common', dirname(__DIR__));
+
+Yii::setAlias('api', dirname(dirname(__DIR__)) . '/api');
+Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
+Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
+Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
